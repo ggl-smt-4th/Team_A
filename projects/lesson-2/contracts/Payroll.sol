045310d0b76a7004a92dfed7e8ad00ea2b3d09d5
Gengran Hu <40301458+endecrypt@users.noreply.github.com>
2018-06-23 11:46:37 -0500

    Update Payroll.sol

--- projects/lesson-2/contracts/Payroll.sol
+++ projects/lesson-2/contracts/Payroll.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.4.14;
 contract Payroll {
 
     struct Employee {
-        // TODO: your code here
+        address id;
+        uint salary;
+        uint lastPayday;
     }
 
     uint constant payDuration = 30 days;
+    uint totalSalary = 0 ether;
 
     address owner;
     Employee[] employees;
@@ -15,27 +18,58 @@ contract Payroll {
         owner = msg.sender;
     }
 
-    function addEmployee(address employeeAddress, uint salary) public {
+    function _partialPaid(Employee employee) private{
+        uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
+        employee.id.transfer(payment);
+    }
+    
+    function _findEmployee(address employeeId) private returns (Employee, uint){
+        for(uint i = 0; i < employees.length; i++){
+            if (employees[i].id == employeeId){
+                return (employees[i], i);
+            }
+        }
+    }
+    
+    function addEmployee(address employeeId, uint salary) public {
         require(msg.sender == owner);
-        // TODO: your code here
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id == 0x0);
+        
+        employees.push(Employee(employeeId, salary * 1 ether, now));
+        totalSalary += salary * 1 ether;
     }
 
     function removeEmployee(address employeeId) public {
         require(msg.sender == owner);
-        // TODO: your code here
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id != 0x0);
+        
+        _partialPaid(employee);
+        totalSalary -= employee.salary;
+        delete employees[index];
+        employees[index] = employees[employees.length - 1];
+        employees.length -= 1;
+        
     }
 
-    function updateEmployee(address employeeAddress, uint salary) public {
+    function updateEmployee(address employeeId, uint salary) public {
         require(msg.sender == owner);
-        // TODO: your code here
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id != 0x0);
+        
+        _partialPaid(employee);
+        totalSalary += (salary  * 1 ether - employee.salary);
+        employees[index].salary = salary * 1 ether;
+        employees[index].lastPayday = now;
     }
 
     function addFund() payable public returns (uint) {
         return address(this).balance;
     }
 
-    function calculateRunway() public view returns (uint) {
-        // TODO: your code here
+    function calculateRunway() public view returns (uint) {        
+        return this.balance / totalSalary; 
     }
 
     function hasEnoughFund() public view returns (bool) {
@@ -43,6 +77,14 @@ contract Payroll {
     }
 
     function getPaid() public {
-        // TODO: your code here
+        var (employee, index) = _findEmployee(msg.sender);
+        assert(employee.id != 0x0);
+        require(this.balance >= employee.salary);
+        
+        uint nextPayday = employee.lastPayday + payDuration;
+        assert(nextPayday < now);
+        
+        employees[index].lastPayday = nextPayday;
+        employee.id.transfer(employee.salary);
     }
 }
