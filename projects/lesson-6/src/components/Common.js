b402e75a74b8f54cbecb6811ebb4411bdbea5f5f
xzf0587 <xzf0587@163.com>
2018-07-08 01:13:43 +0800

    update homework 6

--- projects/lesson-6/src/components/Common.js
+++ projects/lesson-6/src/components/Common.js
@@ -1,59 +1,70 @@
-import React, { Component } from 'react'
-import { Card, Col, Row } from 'antd';
+import React, {Component} from 'react'
+import {Card, Col, Row} from 'antd';
 
 class Common extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {};
-  }
-
-  componentDidMount() {
-    const { payroll } = this.props;
-    const updateInfo = (error, result) => {
-      if (!error) {
-        this.getEmployerInfo();
-      }
+        this.state = {};
     }
 
-    this.getEmployerInfo();
-  }
+    componentDidMount() {
+        const { payroll, web3 } = this.props;
+        const updateInfo = (error, result) => {
+            if (!error) {
+                this.getEmployerInfo();
+            }
+        }
 
-  componentWillUnmount() {
-  }
+        this.addFund = payroll.AddFund(updateInfo);
+        this.getPaid = payroll.GetPaid(updateInfo);
+        this.addEmployee = payroll.AddEmployee(updateInfo);
+        this.updateEmployee = payroll.UpdateEmployee(updateInfo);
+        this.removeEmployee = payroll.RemoveEmployee(updateInfo);
 
-  getEmployerInfo = () => {
-    const { payroll, account, web3 } = this.props;
-    payroll.getEmployerInfo.call({
-      from: account,
-    }).then((result) => {
-      this.setState({
-        balance: web3.fromWei(result[0].toNumber()),
-        runway: result[1].toNumber(),
-        employeeCount: result[2].toNumber()
-      })
-    });
-  }
+        this.getEmployerInfo();
+    }
 
-  render() {
-    const { runway, balance, employeeCount } = this.state;
-    return (
-      <div>
-        <h2>通用信息</h2>
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card title="合约金额">{balance} Ether</Card>
-          </Col>
-          <Col span={8}>
-            <Card title="员工人数">{employeeCount}</Card>
-          </Col>
-          <Col span={8}>
-            <Card title="可支付次数">{runway}</Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+    componentWillUnmount() {
+        this.addFund.stopWatching();
+        this.getPaid.stopWatching();
+        this.addEmployee.stopWatching();
+        this.updateEmployee.stopWatching();
+        this.removeEmployee.stopWatching();
+    }
+
+    getEmployerInfo = () => {
+        const {payroll, account, web3} = this.props;
+        payroll.getEmployerInfo.call({
+            from: account,
+        }).then((result) => {
+            this.setState({
+                balance: web3.fromWei(result[0].toNumber()),
+                runway: result[1].toNumber(),
+                employeeCount: result[2].toNumber()
+            })
+        });
+    }
+
+    render() {
+        const {runway, balance, employeeCount} = this.state;
+        return (
+            <div>
+                <h2>通用信息</h2>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Card title="合约金额">{balance} Ether</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="员工人数">{employeeCount}</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="可支付次数">{runway}</Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Common
